e6afc47175900de1e33777e1252fc4931859f543
Marek Blazek <marek@mblazek.dev>
2020-05-25 23:12:13 +0200

    test

---
+++ precache-manifest.ca556dbeec586fb404a081352d8467dd.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "01b58a081fe8504ee98e",
+    "url": "/css/app.b0093743.css"
+  },
+  {
+    "revision": "8b804935779088a68ec4",
+    "url": "/css/chunk-vendors.78cf6b28.css"
+  },
+  {
+    "revision": "9e2aa52cc2fea6ad14623461ef087136",
+    "url": "/index.html"
+  },
+  {
+    "revision": "01b58a081fe8504ee98e",
+    "url": "/js/app.0f7eac38.js"
+  },
+  {
+    "revision": "8b804935779088a68ec4",
+    "url": "/js/chunk-vendors.d322da05.js"
+  },
+  {
+    "revision": "a209ad586c2db124f580bafb780a7032",
+    "url": "/list.json"
+  },
+  {
+    "revision": "ffc6899f4eba1102ca185442fc706ec2",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "b44917fd9acf7cdcdbf0a6c556a11c40",
+    "url": "/static/docs/1-2020.pdf"
+  },
+  {
+    "revision": "6c2ee5d15cdeeb8114d3b611e985cb9f",
+    "url": "/static/docs/100-2020.pdf"
+  },
+  {
+    "revision": "0bf0f2b3e90f7bb887a7794de98e0887",
+    "url": "/static/docs/SP-2020-1.pdf"
+  }
+]);
