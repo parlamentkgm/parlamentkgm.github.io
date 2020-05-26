543a524baef409c91a251eacf892c00da13967ed
Marek Blazek <marek@mblazek.dev>
2020-05-26 10:01:02 +0200

    V1

---
+++ precache-manifest.33d99077b890b601f7f489a0ccdf281a.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1d36679df3c416de2744",
+    "url": "/css/app.b0093743.css"
+  },
+  {
+    "revision": "83e427ca2e3a621b2e61",
+    "url": "/css/chunk-vendors.3135bc53.css"
+  },
+  {
+    "revision": "faa3abcc3d0788e00155fa9283853e4b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1d36679df3c416de2744",
+    "url": "/js/app.0d84ae33.js"
+  },
+  {
+    "revision": "83e427ca2e3a621b2e61",
+    "url": "/js/chunk-vendors.d79222e2.js"
+  },
+  {
+    "revision": "71db474450b4dfe8549e7e9ae6decd74",
+    "url": "/list.json"
+  },
+  {
+    "revision": "ffc6899f4eba1102ca185442fc706ec2",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
